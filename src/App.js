0da977a293d4c4b937046ffee17345d45f08000c
Wealth Iduwe <wealthiduwe@gmail.com>
2022-01-28 13:47:31 +0100

    learning websockets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:7000');
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState([]);
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    socket.on('message', (payload) => {
+      setChat([...chat, payload]);
+      console.log('Message recieved on server :', payload);
+    });
+  });
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log(message);
+    //send message on sockets
+
+    socket.emit('message', { name: user, message: message });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='mario-chat'>
+        <h2>Mario Chat</h2>
+        <div className='chat-window'>
+          <div className='output'>
+            {chat.map((msg) => (
+              <>
+                <h4>{msg.name}</h4>
+                <p>{msg.message}</p>
+              </>
+            ))}
+          </div>
+        </div>
+        <input
+          className='handle'
+          type='text'
+          placeholder='Handle'
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <input
+          className='message'
+          type='text'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder='Message'
+        />
+        <button onClick={sendMessage} className='send'>
+          Send
+        </button>
+      </div>
     </div>
   );
 }
